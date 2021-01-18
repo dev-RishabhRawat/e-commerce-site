0b34af6709bf867de04c6560e34aa9e41cd21420
Rishabh <rishabhr71@gmail.com>
2021-01-18 12:00:35 +0530

    new commit

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const ProductDescription = (e) =>{
 product_name = this.document.activeElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
 product_price = this.document.activeElement.previousElementSibling.previousElementSibling.previousElementSibling.lastElementChild.textContent;
 document.getElementById('products-container').style.display = "none";
-document.getElementById('product-descrition').style.display = "block";
+document.getElementById('product-descrition').style.display = "flex";
+document.getElementById('header').style.display = "none";
+document.getElementById('product-name').innerHTML = product_name;
+document.getElementById('product-price').innerHTML = product_price;
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
 
 }
