fb450f53758c0690e6b896dc24ec00b064b46c3f
Rishabh <rishabhr71@gmail.com>
2021-01-18 12:20:40 +0530

    now ok

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let product_name,product_price;
 const ProductDescription = (e) =>{
 //     console.log("price : " + this.document.activeElement.previousElementSibling.previousElementSibling.previousElementSibling.lastElementChild.textContent);
 //     console.log("name:" + this.document.activeElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
+//    src: console.log(this.document.activeElement.parentElement.parentElement.firstElementChild.firstElementChild.getAttribute('src'));
 product_name = this.document.activeElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
 product_price = this.document.activeElement.previousElementSibling.previousElementSibling.previousElementSibling.lastElementChild.textContent;
 document.getElementById('products-container').style.display = "none";
@@ -11,5 +12,10 @@ document.getElementById('product-name').innerHTML = product_name;
 document.getElementById('product-price').innerHTML = product_price;
 document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
+let imgSrc = this.document.activeElement.parentElement.parentElement.firstElementChild.firstElementChild.getAttribute('src');
+let img = document.getElementsByClassName('product-image');
+img[0].src = `${imgSrc}`;
+img[1].src = `${imgSrc}`;
+
 
 }
